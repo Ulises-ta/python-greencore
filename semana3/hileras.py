c29fbba7df7cfa8aa0ffeafe2b8a512ea95cbb00
ulises.ta@gmail.com <ulises.ta@gmail.com>
2018-10-01 20:47:09 -0600

    added strings manupulation examples

--- semana3/hileras.py
+++ semana3/hileras.py
@@ -1,1 +1,31 @@
 # Tema Hileras
+
+message = input("Digite lo que quiera: ")
+print(message)
+
+# len() -> Encontrar la cantidad de caracteres que tiene una hilera
+print(len(message))
+
+# Indices
+print(message[0])  # Obtenemos el 1er caracter de una hilera
+print(message[len(message) -1])  # Obtenemos el ultimo caracter de una hilera
+
+# Concatenacion
+hilera = "Hilera inicial"
+
+# Concatenacion simple
+print(hilera + " otra hilera")
+print(hilera)
+
+# Contenacion modificando la variable
+hilera += ". Esto si modifica la variable hilera"
+print(hilera)
+
+# Inyectar texto
+otra_hilera = "Hola {} {}! Como esta? {}".format("Pedro", "Guzman", hilera)
+print(otra_hilera)
+
+# "Slices" / Sub-hileras
+print(otra_hilera[3:6])
+print(otra_hilera[:10])
+print(otra_hilera[10:])
